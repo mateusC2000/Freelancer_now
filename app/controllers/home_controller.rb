5000a31e03b514c9874ff66c36a1d485506d8086
Mateus Campos <mateus.camphos2000@gmail.com>
2021-10-09 11:58:20 -0300

    Project Owner logs in and out of the application.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,15 @@
 class HomeController < ApplicationController
+  layout :change_layout
+  
   def index
     @projects = Project.all
+  end
+
+  private
+
+  def change_layout
+    if project_owner_signed_in?
+      'project_owner'
+    end
   end
 end
---
+++ spec/system/admin/project_owner_account_management_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe 'Project Owner account management' do
+  context 'log in' do
+    it 'successfully' do
+      teu = ProjectOwner.create!(email: 'teu@shelby.com.br', password: '123456789')
+
+      visit root_path
+      click_on 'Área do dono do projeto'
+      fill_in 'Email', with: teu.email
+      fill_in 'Senha', with: teu.password
+      within 'form' do
+        click_on 'Entrar'
+      end
+
+      expect(page).to have_content('Login efetuado com sucesso!')
+      expect(page).to have_content('Área do dono do projeto')
+      expect(page).to have_content(teu.email)
+      expect(page).to have_link('Sair')
+      expect(page).not_to have_link('Área do dono do projeto')
+      expect(page).to have_link('Cadastrar Projeto')
+
+    end
+    it 'there can be no blank fields' do
+
+      visit new_project_owner_session_path
+      within 'form' do
+        click_on 'Entrar'
+      end
+      expect(page).to have_content('Email ou senha inválida.')
+    end
+  end
+end
